c25d7ad7fd3c240af83aac05b6f7764e955c9daf
aryankumar23 <aryankumar2003may@gmail.com>
2024-09-03 17:42:28 +0530

    server_url_update

--- frontend/notes-app/src/components/input/PasswordInput.jsx
+++ frontend/notes-app/src/components/input/PasswordInput.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import {FaRegEye,FaRegEyeSlash} from "react-icons/fa6"
+
 
  const PasswordInput=({value,onChange,placeholder}) =>{
     const [isShowPassword,setIsShowPassword]=useState(false);
--- frontend/notes-app/src/utils/constants.js
+++ frontend/notes-app/src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL="http://localhost:8000"
+export const BASE_URL="https://notify-server-qlgh6ss0n-aryan-kumars-projects-51af7ebd.vercel.app/"
