da6a3d0645bfd3903ee74ed524bd80cd716b0577
aryankumar23 <aryankumar2003may@gmail.com>
2024-09-29 14:50:52 +0530

    cors-update

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
  
 app.use(
     cors({
-        origin: "https://notify-ten-lyart.vercel.app",
+        origin: "notify-ten-lyart.vercel.app",
         credentials: true
     })
 
